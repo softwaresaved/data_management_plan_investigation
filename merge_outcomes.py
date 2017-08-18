e4367e2bf9bd1c5244fd5ffa1bd4cba6fae02368
simonhettrick <s.hettrick@software.ac.uk>
2017-08-18 17:06:21 +0100

    Adding script to merge outcomes so that I can lift it from the main plotting scripts

---
+++ merge_outcomes.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+import pandas as pd
+import numpy as np
+import csv
+import os.path
+
+DATA_FILE_DIR = "./data/"
+
+
+def import_csv_to_df(filename):
+    """
+    Imports a csv file into a Pandas dataframe
+    :params: get an xls file and a sheetname from that file
+    :return: a df
+    """
+    return pd.read_csv(filename)
+
+
+def export_to_csv(df, location, filename):
+    """
+    Exports a df to a csv file
+    :params: a df and a location in which to save it
+    :return: nothing, saves a csv
+    """
+    return df.to_csv(location + filename + '.csv')
+
+
+def get_data_and_merge():
+    """
+    Build dataframes from each of the outcome classes and
+    merge them.
+    :params: nothing
+    :return: a dataframe built from csvs in a folder
+    """
+    
+    dfs_imports = {}
+
+    # Go through all the csvs in the dir, import them into
+    # dfs and then add each one to a dict of dfs
+    for file in os.listdir(DATA_FILE_DIR):
+        if file.endswith('.csv') & (file != 'all_outcomes.csv'):
+            # Create a nice name for each df
+            df_name = file[:-4]
+            dfs_imports[df_name] = import_csv_to_df(DATA_FILE_DIR + str(file))
+    # Merge all the dfs in the dict into a super-df
+    df = pd.concat(dfs_imports.values())
+
+    return df
+
+
+def main():
+    """
+    Main function to run program
+    """
+    
+    df = get_data_and_merge()
+    
+    print(df.columns)
+    
+    export_to_csv(df, DATA_FILE_DIR, 'all_outcomes')
+
+if __name__ == '__main__':
+    main()
+    
